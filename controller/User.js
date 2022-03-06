398a266846b6d1b6b07ebabfdefc69e739b15bdd
hamraz <hamriouz@gmail.com>
2022-03-06 14:54:21 +0330

    debug 2 + clean code

--- controller/User.js
+++ controller/User.js
@@ -2,7 +2,7 @@ const bcrypt = require("bcryptjs");
 const Token = require("../Token");
 const ChangeDetail = require("../controller/ChangeDetail");
 const SeeDetail = require("../controller/SeeDetail");
-
+const UserModel = require('../db/models/User');
 // TODO
 
 let allEmails = [];
@@ -15,34 +15,10 @@ class User {
         allEmails.length = 0;
     }
 
-    static doesEmailExist(email) {
-        let isRepetitive = false;
-
-        allEmails.forEach(checkFunction);
-
-        function checkFunction(given_mail) {
-            if (email === given_mail) {
-                isRepetitive = true;
-            }
-        }
-
-        return isRepetitive;
-    }
-
-    static findObjectByKey(key, value) {
-        for (let i = 0; i < allUsers.length; i++) {
-            if (allUsers[i][key] === value) {
-                return allUsers[i];
-            }
-        }
-        return null;
-    }
-
     static async login(email, password) {
-        // const user = User.findObjectByKey("email", email);
-        const user = await User.query().select('*').where('email', '=', email);
-        const encryptedPassword = await User.query().select("password").where("email", '=',email);
-        const userStatus = await User.query().select("status").where("email", '=',email);
+        const user = await UserModel.query().select('*').where('email', '=', email);
+        const encryptedPassword = await UserModel.query().select("password").where("email", '=',email);
+        const userStatus = await UserModel.query().select("status").where("email", '=',email);
         if (user && bcrypt.compare(password, encryptedPassword)) {
             if (userStatus.toString() === "enable")
                 throw "Your account was disabled! You don't have the permission to log in!"
@@ -50,21 +26,6 @@ class User {
             Token.createToken(user, email)
         } else
             throw "Invalid Credentials!"
-    }
-
-    change_detail(employee, name, familyName, workingHour) {
-        ChangeDetail.changeDetailByEmployee(employee, name, familyName, workingHour);
-    }
-
-    get_all_employee(department) {
-        return SeeDetail.getAllEmployeeDepartmentByEmployee(department, allUsers);
-    }
-
-    see_working_hour(email_address) {
-        let user = User.findObjectByKey("email", email_address)
-        if (!user)
-            throw "Employee with the given Email Address doesn't exist!"
-        return SeeDetail.workingHourByEmployee(user);
     }
 
 }
