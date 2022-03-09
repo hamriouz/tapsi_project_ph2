acd3f5a9ff962c7db04a26e98f1e66eb6065879e
hamraz <hamriouz@gmail.com>
2022-03-09 16:19:39 +0330

    remove queries from changeDetail to database manager

--- controller/ChangeDetail.js
+++ controller/ChangeDetail.js
@@ -1,118 +1,47 @@
-const User = require("../db/models/User");
+const DataBaseManager = require("../db/db-manager/DataBaseManager")
 
 class ChangeDetail {
 
     static async changeDetailByEmployee(email, name, familyName, workingHour) {
-        if (name) {
-            await User.query()
-                .where("email", '=', email)
-                .patch({
-                    name: name
-                });
-        }
-        if (familyName) {
-            await User.query()
-                .where("email", '=', email)
-                .patch({
-                    family_name: familyName
-                });
-        }
-        if (workingHour) {
-            await User.query()
-                .where("email", '=', email)
-                .patch({
-                    working_hour: workingHour
-                });
-        }
+        if (name)
+            await DataBaseManager.changeName(name, email)
+        if (familyName)
+            await DataBaseManager.changeFamilyName(familyName, email)
+        if (workingHour)
+            await DataBaseManager.changeWorkingHour(workingHour, email)
     }
 
     static async changeDetailByAdmin(name, familyName, email, department, organizationLevel, office, workingHour, role, status) {
-        // let employee = UserController.findObjectByKey("email", email);
-        // if (employee !== null) {
         if (name)
-            await User.query()
-                .where("email", '=', email)
-                .patch({
-                    name: name
-                });
+            await DataBaseManager.changeName(name, email)
         if (familyName)
-            await User.query()
-                .where("email", '=', email)
-                .patch({
-                    family_name: familyName
-                });
+            await DataBaseManager.changeFamilyName(familyName, email)
         if (department)
-            await User.query()
-                .where("email", '=', email)
-                .patch({
-                    department: department
-                });
+            await DataBaseManager.changeDepartment(department, email)
         if (organizationLevel)
-            await User.query()
-                .where("email", '=', email)
-                .patch({
-                    organization_level: organizationLevel
-                });
+            await DataBaseManager.changeOrganizationLevel(organizationLevel, email)
         if (office)
-            await User.query()
-                .where("email", '=', email)
-                .patch({
-                    office: office
-                });
-        if (workingHour) {
-            await User.query()
-                .where("email", '=', email)
-                .patch({
-                    working_hour: workingHour
-                });
-        }
+            await DataBaseManager.changeOffice(office, email)
+        if (workingHour)
+            await DataBaseManager.changeWorkingHour(workingHour, email)
         if (role)
-            await User.query()
-                .where("email", '=', email)
-                .patch({
-                    role: role
-                });
+            await DataBaseManager.changeRole(role, email)
         if (status)
-            await User.query()
-                .where("email", '=', email)
-                .patch({
-                    status: status
-                });
-        // } else throw "Employee with the given Email Address doesn't exist!";
+            await DataBaseManager.changeStatus(status, email)
     }
 
     static async changeStateByAdmin(emailAddress) {
         let enOrDis;
-        let employeeStatus = await User.query().select('status').where('email','=',emailAddress)
-        // TODO test status
-        if (employeeStatus['status'] === "enable") {
-            await User.query()
-                .where("email", '=', emailAddress)
-                .patch({
-                    status: 'disable'
-                });
+        let employeeStatus = DataBaseManager.getStatus(emailAddress)
+        if (employeeStatus === "enable") {
+            await DataBaseManager.disable(emailAddress)
             enOrDis = "disabled";
         } else {
+            await DataBaseManager.enable(emailAddress)
             enOrDis = "enabled";
-            await User.query()
-                .where("email", '=', emailAddress)
-                .patch({
-                    status: 'enable'
-                });
         }
         return enOrDis;
     }
 }
 
 module.exports = ChangeDetail;
-
-
-/*async function tester(){
-   await ChangeDetail.changeDetailByEmployee("admin@email.com",undefined,"salam",undefined);
-   return SeeDetail.viewDetailOneEmployeeByAdmin("admin@email.com");
-
-}
-
-tester().then(r => {
-    console.log(r);
-})*/
--- db/db-manager/DataBaseManager.js
+++ db/db-manager/DataBaseManager.js
@@ -1,29 +1,29 @@
 const Admin = require("../models/Admin");
 const User = require("../models/User")
 
-class DataBaseManager{
-    static async getAdmin(){
+class DataBaseManager {
+    static async getAdmin() {
         return Admin.query().select('*').where('role', '=', "admin");
     }
 
-    static async getUserByEmail(email){
+    static async getUserByEmail(email) {
         return User.query().select("*").where('email', '=', email)
     }
 
-    static getRole(email){
+    static getRole(email) {
         return this.getUserByEmail(email)[0].role
 
     }
 
-    static getPassword(email){
+    static getPassword(email) {
         return this.getUserByEmail(email)[0].password
     }
 
-    static getStatus(email){
+    static getStatus(email) {
         return this.getUserByEmail(email)[0].status
     }
 
-    static async addAdmin(email, encryptedPassword, phoneNumber, name, familyName, department, organizationLevel, office, workingHour){
+    static async addAdmin(email, encryptedPassword, phoneNumber, name, familyName, department, organizationLevel, office, workingHour) {
         await Admin.query().insert({
             role: "admin",
             email: email,
@@ -40,7 +40,7 @@ class DataBaseManager{
 
     }
 
-    static async addEmployee(role, email, encryptedPassword, phoneNumber, familyName, department, organizationLevel, office, workingHour, status){
+    static async addEmployee(role, email, encryptedPassword, phoneNumber, familyName, department, organizationLevel, office, workingHour, status) {
         await User.query().insert({
             role: role,
             email: email,
@@ -56,7 +56,7 @@ class DataBaseManager{
         })
     }
 
-    static async listEmployeeAdmin(){
+    static async listEmployeeAdmin() {
         return User.query().select(
             'name',
             'family_name',
@@ -64,7 +64,7 @@ class DataBaseManager{
             'office');
     }
 
-    static async detailEmployeeAdmin(email){
+    static async detailEmployeeAdmin(email) {
         return User.query().select(
             'name',
             'family_name',
@@ -79,7 +79,7 @@ class DataBaseManager{
         ).where("email", '=', email)
     }
 
-    static async allEmployeeDepartment(department){
+    static async allEmployeeDepartment(department) {
         return User.query().select(
             'email',
             'name',
@@ -87,12 +87,90 @@ class DataBaseManager{
             .where("department", '=', department)
     }
 
-    static async workingHour(email){
+    static async workingHour(email) {
         return User.query().select("working_hour").where('email', '=', email);
 
     }
 
+    static async changeName(name, email) {
+        await User.query()
+            .where("email", '=', email)
+            .patch({
+                name: name
+            });
+    }
 
+    static async changeFamilyName(familyName, email) {
+        await User.query()
+            .where("email", '=', email)
+            .patch({
+                family_name: familyName
+            });
+    }
+
+    static async changeDepartment(department, email) {
+        await User.query()
+            .where("email", '=', email)
+            .patch({
+                department: department
+            });
+    }
+
+    static async changeOrganizationLevel(organizationLevel, email) {
+        await User.query()
+            .where("email", '=', email)
+            .patch({
+                organization_level: organizationLevel
+            });
+    }
+
+    static async changeOffice(office, email) {
+        await User.query()
+            .where("email", '=', email)
+            .patch({
+                office: office
+            });
+    }
+
+    static async changeWorkingHour(workingHour, email) {
+        await User.query()
+            .where("email", '=', email)
+            .patch({
+                working_hour: workingHour
+            });
+    }
+
+    static async changeRole(role, email) {
+        await User.query()
+            .where("email", '=', email)
+            .patch({
+                role: role
+            });
+    }
+
+    static async changeStatus(status, email) {
+        await User.query()
+            .where("email", '=', email)
+            .patch({
+                status: status
+            });
+    }
+
+    static async enable(email) {
+        await User.query()
+            .where("email", '=', email)
+            .patch({
+                status: 'enable'
+            });
+    }
+
+    static async disable(email) {
+        await User.query()
+            .where("email", '=', email)
+            .patch({
+                status: 'disable'
+            });
+    }
 }
 
 module.exports = DataBaseManager
